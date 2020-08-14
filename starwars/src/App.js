7eb156ad9aee314e9db1ddcf0d59669889cd4b2f
saljahmi <saljahmi@gmail.com>
2020-08-14 12:18:29 -0400

    completed app function, some of character.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useEffect} from 'react'
+import axios from 'axios';
 import './App.css';
+import styled from "styled-components";
+
+import Character from './components/Character'
+
+const Characters = styled.ul`
+  width: 80%;
+  margin: 0 auto;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +19,27 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/?')
+      .then(res => {
+        console.log(res.data.results)
+        setCharacters(res.data.results)
+      })
+      .catch(err => {
+        debugger
+      })
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {/* <Characters>
+        {characters.map((char) => (
+          <Character key={char.name} endpoint={char.url} />
+        ))}
+      </Characters> */}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,52 @@
 // Write your Character component here
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+const Card = styled.li`
+  list-style: none;
+  background-color: darkseagreen;
+  box-sizing: border-box;
+  padding: 0.5rem;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default function Character({ endpoint }) {
+    const [charData, setCharData] = useState({});
+    const [showDetails, setShowDetails] = useState(false);
+  
+    const handleClick = () => {
+      setShowDetails(!showDetails);
+    };
+  
+    useEffect(() => {
+      axios
+        .get(endpoint)
+        .then((res) => {
+          setCharData(res.data);
+        })
+        .catch((err) => {
+          console.log("Error", err);
+        });
+    }, [endpoint]);
+
+    return (
+        <Card onClick={() => handleClick()}>
+            <h2>{charData.name}</h2>
+            {showDetails && (
+                <div>
+                    <p>Birth Year: {charData.birth_year}</p>
+                    <p>Mass: {charData.mass}</p>
+                    <p>Height: {charData.height}</p>
+                    <p>Eye Color: {charData.eye_color}</p>
+                    <p>Hair Color: {charData.hair_color}</p>
+                    <p>Gender: {charData.gender}</p>
+                </div>
+            )}
+        </Card>
+    );
+}
